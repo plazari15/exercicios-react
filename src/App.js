8f501f1db64cbdddd236b0bff04c023e23f8a106
pedro <pedro.lazari@adias.com.br>
2019-04-07 22:55:36 -0300

    App Ok

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Simples from './componentes/Simples';
 import ParImpar from './componentes/ParImpar';
-// import { Inverter, MegaSena } from './componentes/Multi';
+import { Inverter, MegaSena } from './componentes/Multi';
 
 export default class App extends Component {
 	render() {
@@ -10,8 +10,8 @@ export default class App extends Component {
 			<View style={styles.container}>
 				<Simples texto='Flexivel' />
 				<ParImpar numero={31}></ParImpar>
-				{/* <Inverter texto='React Nativo' />
-				<MegaSena numeros={6} /> */}
+				<Inverter texto='React Nativo' />
+				<MegaSena numeros={6} />
 			</View>
 		)
 	}
